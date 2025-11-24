b5305fde296114453664bd2214c56f723c598b7c
lmy <1052635979@qq.com>
2025-11-24 23:00:35 +0800

    完成player移动

---
+++ player.py
@@ -0,0 +1,52 @@
+import pygame
+from settings import *
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+
+        # 通用属性
+        self.image = pygame.Surface((32, 64))
+        self.image.fill('green')
+        self.rect = self.image.get_rect(center=pos)
+
+        # 移动属性
+        self.direction = pygame.math.Vector2()
+        self.pos = pygame.math.Vector2(self.rect.center)
+        self.speed = 200
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        elif keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
+
+    def move(self, dt):
+        # 向量归一化
+        if self.direction.magnitude() > 0:  # 模大于1
+            self.direction = self.direction.normalize()
+            print(self.direction)
+
+        # 水平移动更新
+        self.pos.x += self.direction.x * self.speed * dt
+        self.rect.centerx = int(self.pos.x)
+
+        # 垂直移动更新
+        self.pos.y += self.direction.y * self.speed * dt
+        self.rect.centery = int(self.pos.y)
+
+    def update(self, dt):
+        self.input()
+        self.move(dt)
